2d63030315a3242896bc496be838f32130016589
Asid-Ashi <asidashi98@gmail.com>
2021-07-07 10:26:11 +0530

    variables renamed,file created for constants,inline styles replaced by stylesheet

--- App.js
+++ App.js
@@ -1,16 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import { StyleSheet, TextInput, View, Text, Alert, ScrollView,ImageBackground } from 'react-native';
-import { image} from './assets/constants';
+import { StyleSheet, TextInput, View, Text, Alert, ScrollView, ImageBackground } from 'react-native';
+import { image } from './assets/constants';
 
 
 
 export default function App() {
-  const [Name, setName] = useState([])
+  const [Employees, setEmployees] = useState([])
   const [backUp, setbackUp] = useState([])
   const [Loading, setLoading] = useState(true)
-  const [text, settext] = useState("")
+  const [searchInput, setsearchInput] = useState("")
 
   const fetchData = () => {
     axios({
@@ -19,7 +19,7 @@ export default function App() {
 
     }).then((Response) => {
       setbackUp(Response.data.data)
-      setName(Response.data.data)
+      setEmployees(Response.data.data)
       setLoading(false)
 
     })
@@ -32,45 +32,45 @@ export default function App() {
   }, [])
 
   useEffect(() => {
-    if (text.length >= 3) {
-      setName(searching(text))
+    // if (searchInput.length >= 1) {
+    setEmployees(searching(searchInput))
+    // }
+    if (searchInput.length === 0) {
+      setEmployees(backUp)
     }
-    if (text.length === 0) {
-      setName(backUp)
-    }
-  }, [text])
+  }, [searchInput])
 
   const searching = (String) => {
     if (String === '') {
       return (backUp)
     }
     const regex = new RegExp(`${String.trim()}`, 'i');
-    return Name.filter(
-      (Name => Name.employee_name.search(regex) >= 0
+    return Employees.filter(
+      (Employees => Employees.employee_name.search(regex) >= 0
       ));
   }
   return (
     <View >
       <ImageBackground source={image} style={styles.imageStyle}>
-      <View style={styles.container}>
-        <Text style={styles.textStyle2}>Employees List</Text>
+        <View style={styles.container}>
+          <Text style={styles.textStyle2}>Employees List</Text>
 
-        <TextInput style={styles.inputStyle} placeholder={"search name"} value={text} onChangeText={(text) => settext(text)}></TextInput>
-      </View>
+          <TextInput style={styles.inputStyle} placeholder={"search name"} value={searchInput} onChangeText={(searchInput) => setsearchInput(searchInput)}></TextInput>
+        </View>
 
-      <ScrollView style={styles.scrollViewStyle}>
-        {
-          !Loading ? (Name && Name.map((item) => {
-            return (
-              <View style={styles.container1}>
-                <Text style={styles.textStyle2} >{item.employee_name}</Text>
-              </View>
-            )
-          })) : (<Text style={styles.textStyle1}>Loading...</Text>)
-        }
+        <ScrollView style={styles.scrollViewStyle}>
+          {
+            !Loading ? (Employees && Employees.map((item, index) => {
+              return (
+                <View style={styles.container1} >
+                  <Text style={styles.textStyle2} >{index + 1} - {item.employee_name}</Text>
+                </View>
+              )
+            })) : (<Text style={styles.textStyle1}>Loading...</Text>)
+          }
 
-      </ScrollView>
-      <StatusBar style="auto" />
+        </ScrollView>
+        <StatusBar style="auto" />
       </ImageBackground>
     </View>
   );
@@ -78,16 +78,21 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
-     flexDirection: 'row',
-      marginTop:50
+    flexDirection: 'row',
+    marginLeft: 20,
+    marginTop: 50,
+    backgroundColor: '#bababa',
+    height: 50,
+    alignItems: 'center',
+    alignContent: 'center',
+    width: 350
   },
   container1: {
-    backgroundColor: '#bababa',
+
+    backgroundColor: '#abdaca',
     marginBottom: 2,
     width: 350,
     height: 50,
-    alignItems: 'center',
-    alignContent: 'center'
 
   },
   inputStyle: {
@@ -102,9 +107,10 @@ const styles = StyleSheet.create({
     marginLeft: 20
   },
   scrollViewStyle:
-    { marginTop: 20,
-      marginLeft:20
-    },
+  {
+    marginTop: 20,
+    marginLeft: 20
+  },
 
   textStyle: {
     fontSize: 20,
@@ -115,11 +121,11 @@ const styles = StyleSheet.create({
     color: 'red'
   },
   textStyle2: {
-    marginLeft:10,
+    marginLeft: 10,
     color: 'black',
     fontSize: 20
   },
-  imageStyle:{
+  imageStyle: {
     resizeMode: 'cover',
     justifyContent: 'center',
   }
